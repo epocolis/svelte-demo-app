5a2bcc599e23fb5718feae38864d0265ecfe625c
Leotis <my_disposable@tutanota.com>
2022-02-13 19:21:36 -0500

    complete feedback app

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,17 @@
+<script> 
+
+import {fade, scale} from 'svelte/transition'; 
+import {FeedbackStore} from '../stores'
+import FeedbackItem from "./FeedbackItem.svelte";
+
+
+
+
+</script>
+
+
+{#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:fade="{{ duration: 500 }}">
+  <FeedbackItem item={fb} />
+</div>
+{/each}
